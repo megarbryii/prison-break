ddf50be83941d0f4154d5447463e0fe6b788562a
megarbry <megarbry@gmail.com>
2020-05-06 13:57:46 -0600

    Set up DB and server link

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
 const express = require('express');
+
+const connectDB = require('./config/db');
+
+const app = express();
+
+//Connect to the DB
+connectDB();
+
+//Init middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.send('API running');
+});
+
+//Define routes
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
