52f9eaf1d03eb71f94f3d44c6ab39742a7951a58
megarbry <megarbry@gmail.com>
2020-05-06 14:28:20 -0600

    Added Room and User models

---
+++ models/Room.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//Create room schema
+const RoomSchema = new Schema({
+    room_id: {
+        type: Number,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    desc: {
+        type: String,
+        required: true
+    },
+    choice: [
+        {
+            choiceText: {
+                type: String,
+                required: true
+            },
+            nextRoom: {
+                type: Number,
+                required: true
+            }
+        }
+    ]
+});
+
+module.exports = Room = mongoose.model('room', RoomSchema);
