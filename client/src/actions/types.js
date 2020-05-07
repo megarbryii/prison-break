e4272d7e4e0922b5af02d19cf2ad1c867125164e
megarbry <megarbry@gmail.com>
2020-05-07 14:10:23 -0600

    Setup action/types and combineReducers

---
+++ client/src/actions/types.js
@@ -0,0 +1,19 @@
+//Auth
+export const REGISTER_SUCESS = 'REGISTER_SUCESS';
+export const REGISTER_FAIL = 'REGISTER_FAIL';
+export const USER_LOADED = 'USER_LOADED';
+export const AUTH_ERROR = 'AUTH_ERROR';
+export const LOGIN_SUCESSFUL = 'LOGIN_SUCESSFUL';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+export const LOGOUT = 'LOGOUT';
+
+//Rooms
+export const GET_ROOM = 'GET_ROOM';
+export const ROOM_ERROR = 'ROOM_ERROR';
+export const CLEAR_ROOM = 'CLEAR_ROOM';
+export const UPDATE_ROOM = 'UPDATE_ROOM';
+export const DELETE_ROOM = 'DELETE_ROOM';
+
+//Alerts (?)
+export const SET_ALERTS = 'SET_ALERTS';
+export const REMOVE_ALERTS = 'REMOVE_ALERTS';
