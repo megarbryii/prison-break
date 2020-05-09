e72fe8627535ac3e77179352c1285834170c72f8
megarbry <megarbry@gmail.com>
2020-05-08 18:04:58 -0600

    Added createRoom action to game actions

--- client/src/actions/game.js
+++ client/src/actions/game.js
@@ -23,3 +23,39 @@ export const getRoom = (roomId) => async dispatch => {
         });
     }
 }
+
+//Create or update room
+export const createRoom = (formData, history, edit = false) => async dispatch => {
+    try {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        const res = await axios.post('/api/room', formData, config);
+
+        dispatch({
+            type: GET_ROOM,
+            payload: res.data
+        });
+
+        dispatch(setAlert(edit ? 'Room Updated' : 'Room Created', 'success'));
+
+        if(!edit) {
+            //todo: Create rooms page
+            //history.push('/');
+        }
+    } catch (err) {
+        const errors = err.response.data.error;
+
+        if(errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+
+        dispatch({
+            type: ROOM_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+}
